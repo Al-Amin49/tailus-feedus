cf86455d8fb98c86823713fdf6410d45a45d8638
Al-Amin49 <alamin496832@gmail.com>
2024-11-23 12:16:19 +0600

    feat: fetch recipie details page

--- app/all-recipes/page.jsx
+++ app/all-recipes/page.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+"use client";
+import HttpKit from "@/common/helpers/HttpKit";
+import RecipeCard from "@/components/Recipes/RecipeCard";
+import { useQuery } from "@tanstack/react-query";
+import React, { useEffect, useState } from "react";
 
 const AllRecipes = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["recipes"],
+    queryFn: HttpKit.getTopRecipes,
+  });
+
+  useEffect(() => {
+    if (data) {
+      setRecipes(data);
+    }
+  }, [data]);
   return (
-    <div className="bg-gray-50 min-h-screen flex items-center">
-      <div className="container mx-auto">
-        <h1 className="text-4xl">This is the all recipes page</h1>
+    <div className="pt-20 ">
+  
+      <div className="relative py-16">
+        <div className="container relative m-auto px-6 text-gray-500 md:px-12">
+          <div className="grid gap-6 md:mx-auto md:w-8/12 lg:w-full lg:grid-cols-3">
+            {recipes?.map((recipe) => (
+              <RecipeCard
+                key={recipe?.id}
+                recipe={recipe}
+               
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
